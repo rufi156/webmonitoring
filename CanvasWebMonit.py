3009ed4d7730410c69377cd4bc3254377a1c483c
RafałRobert <33355829+rufi156@users.noreply.github.com>
2024-02-02 14:53:59 +0100

    Create CanvasWebMonit.py
    change discord library from discord-webhook to discordwebhook
    
    add save website to html for confirming if the website is downloaded properly and does not depend on dynamic content
    
    simplify discord notification
    
    secure webook management

---
+++ CanvasWebMonit.py
@@ -0,0 +1,43 @@
+import requests
+import hashlib
+import time
+from discordwebhook import Discord
+
+DISCORD_WEBHOOK_URL = ''
+with open('webhook.txt', 'r') as f:
+    DISCORD_WEBHOOK_URL = f.readline()
+
+DISCORD = Discord(url=DISCORD_WEBHOOK_URL)
+
+WEBSITE_URL = 'https://canvas-student.securerc.co.uk/onlineleasing/canvas-utrecht/floorplans.aspx'
+
+# Function to get the MD5 hash of a webpage content
+def get_page_hash(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+    # Save the HTML content to a file
+        with open('output.html', 'w', encoding='utf-8') as file:
+            file.write(response.text)
+        print('HTML content saved to output.html')
+    else:
+        print(f'Error: {response.status_code}')
+    return hashlib.md5(response.content).hexdigest()
+
+# Function to send a Discord notification
+
+# Initial hash value for the first run
+current_hash = get_page_hash(WEBSITE_URL)
+
+while True:
+    # Fetch the current hash value
+    new_hash = get_page_hash(WEBSITE_URL)
+
+    # Check for changes
+    #if new_hash != current_hash:
+    if new_hash == current_hash:
+        DISCORD.post(content=f'Website content not changed: {WEBSITE_URL}')
+        #current_hash = new_hash
+
+    # Wait for 30 minutes before checking again
+    #time.sleep(1800)
+    time.sleep(10)
