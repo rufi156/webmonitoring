48c1d7777e549bc5ab92ff481fe63f2bdd107130
RafałRobert <33355829+rufi156@users.noreply.github.com>
2024-02-02 15:40:26 +0100

    Update CanvasWebMonit.py
    saving the baseline as well as the current website

--- CanvasWebMonit.py
+++ CanvasWebMonit.py
@@ -11,7 +11,7 @@ with open('webhook.txt', 'r') as f:
 WEBSITE_URL = 'https://canvas-student.securerc.co.uk/onlineleasing/canvas-utrecht/floorplans.aspx'
 
 # Function to get the MD5 hash of a webpage content using Selenium
-def get_page_hash(url):
+def get_page_hash(url, save_to_file=False, filename='output.html'):
     options = Options()
     options.headless = True
     driver = webdriver.Chrome(options=options)
@@ -21,32 +21,34 @@ def get_page_hash(url):
         # Wait for dynamic content to load (adjust the sleep duration accordingly)
         time.sleep(5)
         page_content = driver.page_source.encode('utf-8')
-        return hashlib.md5(page_content).hexdigest()
+        current_hash = hashlib.md5(page_content).hexdigest()
+
+        # Save the content to a file if specified
+        if save_to_file:
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write(driver.page_source)
+
+        return current_hash
     finally:
         driver.quit()
 
 if __name__ == "__main__":
     discord = Discord(url=DISCORD_WEBHOOK_URL)
+    print(DISCORD_WEBHOOK_URL)
+    print(discord)
 
     # Initial request before entering the loop
-    initial_hash = get_page_hash(WEBSITE_URL)
-
-    # Save the initial HTML to a file
-    with open('initial_output.html', 'w', encoding='utf-8') as file:
-        options = Options()
-        options.headless = True
-        driver = webdriver.Chrome(options=options)
-        try:
-            driver.get(WEBSITE_URL)
-            file.write(driver.page_source)
-        finally:
-            driver.quit()
+    initial_hash = get_page_hash(WEBSITE_URL, save_to_file=True, filename='initial_output.html')
 
     while True:
-        current_hash = get_page_hash(WEBSITE_URL)
+        current_hash = get_page_hash(WEBSITE_URL, save_to_file=True, filename='current_output.html')
 
-        if current_hash and current_hash != initial_hash:
+        if not current_hash: discord.post(content=f'Error: Cannot retrieve website {WEBSITE_URL}')
+        
+        if current_hash != initial_hash:
             discord.post(content=f'Website content changed: {WEBSITE_URL}')
-            # No need to update the initial hash/template
+        else:
+            discord.post(content=f'Website content not changed: {WEBSITE_URL}')
 
-        time.sleep(1800)
+        #time.sleep(1800)
+        break
